9a194b388a7289952ae1a35f3b49315882c03aea
Elton Zheng <elton.zheng.sh@gmail.com>
2020-08-14 11:46:51 +0800

    Implement CRUD API for user resources.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
 // dependencies here
-use actix_web::{web, App, HttpServer};
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+use actix_web::{dev::ServiceRequest, web, App, Error, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+use dotenv::dotenv;
+use std::env;
 
 // module declaration here
+mod errors;
 mod handlers;
+mod models;
+mod schema;
+
+// type declarations here
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=debug");
+    dotenv().ok();
+
+    // for (key, value) in env::vars() {
+    //     println!("{}: {}", key, value);
+    // }
+    env::set_var("RUST_LOG", "actix_web=debug");
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    // create db connection pool
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
 
     // Start http server
     HttpServer::new(move || {
         App::new()
+            .data(pool.clone()) // This enables the handler functions to interact with the database independently.
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
             .route("/users", web::post().to(handlers::add_user))
